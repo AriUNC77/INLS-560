d861d0bcd91696868d22b16a9d2d5e8bd8d98716
AriUNC77 <smithari96@gmail.com>
2024-08-30 12:47:13 -0400

    Add files via upload

---
+++ 79_char_ruler.py
@@ -0,0 +1,18 @@
+#Smith, Ari
+#This is a screenshot of VSC with a 79 character ruler in use.
+
+#PEP 8's style guide says to limit all lines to 79 characters.
+max_char = 79
+
+#This will print out the number 79.
+print(max_char)
+
+#This will print out the number 0 as a string 79 times.
+print('0' * max_char)
+
+'''
+0000000000000000000000000000000000000000000000000000000000000000000000000000000
+'''
+
+#This will print out the integer 0.
+print(0 * 79)
