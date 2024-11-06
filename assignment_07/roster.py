a808194bdffe1285f7055a6ffd7af182e573a780
Ari Smith <smithari96@gmail.com>
2024-11-06 15:30:21 -0500

    commit zero header

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -1,9 +1,8 @@
 # uses data from https://goheels.com/sports/mens-basketball/roster
 
+import pandas as pd
+
 roster = ['jalen washington', 'james brown', 'jae\'lyn winters','ven-allen lubin']
+data = pd.DataFrame(roster)
 
-print(roster)
-
-print("Here are the tallest UNC basketball players:")
-for player in roster:
-    print(f"{player.title()}")
+print(data)
