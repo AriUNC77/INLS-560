45366f0887dd04c9305dbb73fe7f8895d8cb2ee2
Ari Smith <smithari96@gmail.com>
2024-11-06 15:23:20 -0500

    adding assignment_07 and 09 to my git repository

---
+++ assignment_07/roster.py
@@ -0,0 +1,9 @@
+# uses data from https://goheels.com/sports/mens-basketball/roster
+
+roster = ['jalen washington', 'james brown', 'jae\'lyn winters','ven-allen lubin']
+
+print(roster)
+
+print("Here are the tallest UNC basketball players:")
+for player in roster:
+    print(f"{player.title()}")
---
+++ assignment_07/roster_loop.py
@@ -0,0 +1,7 @@
+# uses data from https://goheels.com/sports/mens-basketball/roster
+
+roster = ['jalen washington', 'james brown', 'jae\'lyn winters','ven-allen lubin']
+
+print("These are the tallest UNC basketball players, from tallest to shortest:")
+for player in roster:
+    print(f"{player.title()}")
