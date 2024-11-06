be3fc2d756db53c850c2180bedf243e4d5f94c7e
Ari Smith <smithari96@gmail.com>
2024-11-06 17:52:38 -0500

    made a csv file for the bmi and added 10 players

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -1,13 +1,19 @@
 # uses data from https://goheels.com/sports/mens-basketball/roster
 
 import pandas as pd
-
-player = {"First Name": ['Washington', 'Brown', 'Winters'],
-        "Last Name": roster,
-          "First Name": ['Jalen', 'James',"Jae'Lyn"],
-          'Height':[70, 70, 69],
-          'Weight':[235, 215, 220]}
+print("The following is a list of the top ten tallest UNC basketball players:\n")
+player = {"First Name": ['Jalen', 'James',"Jae'Lyn", 'Ven-Allen', "John",
+                         "Cade", "Ty", "Drake", "Ian", "Seth"],
+          "Last Name": ['Washington', 'Brown', 'Winters', "Lubin", 'Holbrook',
+                        'Tyson','Claude', "Powell", "Jackson", "Trimble"],
+          'Height':[70, 70, 69, 68, 68, 67, 67, 66, 64, 63],
+          'Weight':[235, 215, 220, 230, 230, 200, 230, 195, 190, 195]}
 
 data = pd.DataFrame(player)
 
+# bmi is height in kg/weight in meters squared
+data["BMI"] = round((data["Weight"]/2.205)/((data["Height"]/39.37)**2), 2)
+
 print(data)
+
+data.to_csv("bmi.csv")
