a655ba8a4afd279967101adfd24f812674af9bf4
Ari Smith <smithari96@gmail.com>
2024-11-06 15:39:23 -0500

    dictionary expanded

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 
-roster = ['Washington', 'Brown', 'Winters','Lubin']
-player = {"Last Name": roster}
+roster = ['Washington', 'Brown', 'Winters']
+player = {"Last Name": roster,
+          "First Name": ['Jalen', 'James',"Jae'Lyn"],
+          'Height':[70, 70, 69],
+          'Weight':[235, 215, 220]}
 
 data = pd.DataFrame(player)
 
